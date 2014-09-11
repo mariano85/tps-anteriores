07cf29dd81d3e5cfc05b8ed5a2ef40f9b54af629
Messa Nicolas <nnico15m@users.noreply.github.com>
2014-09-11 01:12:30 -0300

    Un par de funciones de la cpu

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -14,13 +14,29 @@
 //////////////////////////Main principal ////////////////////////
 ///////////////////////////////////////////////////////////////////
 
+#include "variablesGlobales_Funciones.h"
+
 int main(int argc, char** argv) {
 
+	argc = 2; //Le paso un parametro asi no pincha
+	**argv = 'a';
 
 	logs = log_create("log", "CPU.c", 1, LOG_LEVEL_TRACE);
-	config = config_create("cpu.conf");
 
-	diccionarioDeVariables = dictionary_create();
+	if (argc < 2){
+			log_error(logs, "No se pasaron parametros.");
+			log_destroy(logs);
+			return 0;
+		}
+
+		A = malloc(sizeof(int));
+		B = malloc(sizeof(int));
+		C = malloc(sizeof(int));
+		D = malloc(sizeof(int));
+		E = malloc(sizeof(int));
+
+	void inicializar_CPU();
+
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	////////////// Falta verificar que el archivo de configuracion sea valido //////////////////////
@@ -30,16 +46,32 @@ int main(int argc, char** argv) {
 	/*Al iniciar,intentara conectarse a los procesos kernel y msp,en caso de no poder
 conctarse,abortara su ejecucion informando por pantalla y el log del motivo*/
 
-	socketKernel = conectar_kernel();
-	socketMSP =conectar_MSP();
+//	socketKernel = conectar_kernel();
+	//socketMSP =conectar_MSP();
+	//	if(socketKernel < 0 || socketMSP < 0){
+	//		log_error(logs, "El programa tuvo que finalizar insatisfactoriamente porque fallo alguna conexion");
+			//liberar_estructuras();
+	//		return 0;}
 
-	if(socketKernel < 0 || socketMSP < 0){
-		log_error(logs, "El programa tuvo que finalizar insatisfactoriamente porque fallo alguna conexion");
-		//liberar_estructuras();
-		return EXIT_FAILURE;
-	}
 
-	return EXIT_SUCCESS;
+
+ // pthread_create(&pthread_Consola,NULL,manejo_consola,NULL);
+
+  	  	LOAD(A,12);
+		printf("%d\n",*A);
+		GETM(D,A);
+		printf("%d\n",*D);
+		ADDR(D,A);
+		printf("%d\n",*D);
+
+
+		free(A);
+		free(B);
+		free(C);
+		free(D);
+		free(E);
+
+		return EXIT_SUCCESS;
 }
 
 
@@ -48,26 +80,25 @@ conctarse,abortara su ejecucion informando por pantalla y el log del motivo*/
 //////////////////////// Funciones ///////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 
-
-void interpretar_sentencia_ESO(int bytecode,char registro,int numero){
-
-
-	switch (bytecode) {
-
-		case 1297045074: //Si es LOAD
-
-			registro = numero;
-
-				break;
-
-	}
+void LOAD(int *Registro,int Numero){
 
 
 
+	*Registro = Numero;
 
+}
 
-	}
+void GETM(int *Registro1,int *Registro2){
 
+	*Registro1 = *Registro2;
+
+}
+
+void ADDR(int *Registro1,int *Registro2){
+
+	*Registro1 = *Registro1 + *Registro2;
+
+}
 
 int conectar_kernel(){
 
@@ -86,5 +117,18 @@ int conectar_MSP(){
 	return 0; //Por ahora devuelvo 0 porque no defini lo que hace esta funcion
 
 }
+
+
+void inicializar_CPU(){
+
+
+
+
+
+}
+
+
+
+
 
 
--- cpu/cpu.h
+++ cpu/cpu.h
@@ -5,9 +5,12 @@
  *      Author: utnso
  */
 
-#ifndef PROYECTOPRACTICA_H_
-#define PROYECTOPRACTICA_H_
+#ifndef VARIABLESGLOBALES_FUNCIONES_H_
+#define VARIABLESGLOBALES_FUNCIONES_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/temporal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/bitarray.h>
@@ -20,20 +23,37 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
 
 t_log *logs;
 t_config* config;
 t_dictionary* diccionarioDeVariables;
 
+//Registro CPU despues veremos como los agrupamos
 
-int registroA;
-int registroB;
+int  *A,*B,*C,*D,*E;
+
 int socketKernel;
 int socketMSP;
+int PUERTO_KERNEL,PUERTO_MSP;
+pthread_t pthread_Consola;
 
-#endif /* PROYECTOPRACTICA_H_ */
 
-void interpretar_sentencia_ESO(int a,char b,int c);
+
+
 int conectar_kernel();
 int conectar_MSP();
+int  p_HILO;
 
+void inicializar_Configuracion();
+
+void LOAD(int *Registro,int Numero);
+
+void GETM(int *Registro1,int *Registro2);
+
+void ADDR(int *Registro1,int *Registro2);
+
+void *manejo_consola(); //Voy a crear una mini consola para probar las cosas
+
+
+#endif /* VARIABLESGLOBALES_FUNCIONES_H_ */
