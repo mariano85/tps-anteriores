54473bba9448a2cd7b72c9860b8b13c1f3b5f21c
mariano85 <mariano.umbides@gmail.com>
2014-10-15 23:32:35 -0300

    kernel: version que compila. limpiar kernel.h y borrar los .c que no sirven

--- Kernel/kernel.h
+++ Kernel/kernel.h
@@ -8,36 +8,30 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include "funciones_manejo_cola.h"
-#include "global.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/collections/queue.h>
+#include <commons/collections/list.h>
+#include <semaphore.h>
 
 #define KERNEL_CONF "config.conf"
-#define MSG_SIZE 256
-#define QUANTUM "QUANTUM"
 
 t_queue *NEW;
 t_queue *READY;
 t_queue * EXEC;
 t_queue *EXIT;
 t_queue *BLOCK;
-
 sem_t mutexNEW;
 sem_t mutexREADY;
 sem_t mutexEXEC;
 sem_t mutexEXIT;
 sem_t mutexBLOCK;
 
-
-pthread_mutex_t mutex_new_queue;
-pthread_mutex_t mutex_ready_queue;
-pthread_mutex_t mutex_block_queue;
-pthread_mutex_t mutex_exec_queue;
-pthread_mutex_t mutex_exit_queue;
-
-pthread_mutex_t mutexCPUDISP;
-
-t_log *kernel_log;
+sem_t mutexCPUDISP;
+t_log *archivo_logs;
 t_log *queue_log;
 t_config *config;
 
@@ -45,7 +39,6 @@ t_list *cpu_disponibles_list;
 
 bool archivo_configuracion_valido();
 void conectarse_Planificador();
-
 
 
 t_dictionary *semaforos;
@@ -57,7 +50,31 @@ typedef struct t_semaforos {
 	sem_t mutex;
 } t_semaforos;
 
+/*
+ * headers del manejo del tcb
+ */
 
+#define MODO_KERNEL 1
+#define MODO_USUARIO 0
+
+typedef struct s_tcb {
+    int pid;
+
+	int tid;
+
+	int indicador_modo_kernel;
+	int base_segmento_codigo;
+	int tamanio_indice_codigo ;
+	int indice_codigo;
+	int program_counter;
+	int puntero_instruccion;
+	int base_stack;
+	int cursor_stack;
+
+	int reg_programacion;
+
+
+} t_tcb;
 
 typedef struct s_client_cpu{
 	int32_t processFd;
@@ -67,11 +84,28 @@ typedef struct s_client_cpu{
 	bool ocupado;
 }t_client_cpu;
 
-typedef char t_contenido[MSG_SIZE];
+/*
+ * headers del manejo de cola
+ */
 
-// PRUEBA
+t_log* queueLog;
 
-TCB *tcb1,*tcb2,*tcb3,*tcb4;
+void* sacarCola(t_queue* cola_actual, sem_t *mutex, sem_t *hay_algo_para_sacar);
+void ponerCola(t_tcb *unPCB, t_queue* cola_actual, sem_t *mutex, sem_t *hay_algo_para_sacar);
+
+void mostrarColas();
+void mostrarCola(t_queue* aQueue, sem_t queueMutex, t_log* logger);
+
+
+void agregarProceso(t_tcb* aProcess);
+void agregarProcesoColaReady(t_tcb* aProcess);
+void agregarProcesoColaExec();
+void manejo_cola_ready(void);
+void chequearProcesos();
+int32_t cantDeProcesosEnSistema();
+
+pthread_cond_t cond_exit_consumer, cond_exit_producer,cond_ready_consumer, cond_ready_producer, condpBlockedProcess;
+
 
 
 
