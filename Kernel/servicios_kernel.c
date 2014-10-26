47a8bed4a098f502438d63fd9e4df21dc3992c77
mariano85 <mariano.umbides@gmail.com>
2014-10-26 16:44:52 -0300

    se arreglan problemas de compilacion

--- Kernel/servicios_kernel.c
+++ Kernel/servicios_kernel.c
@@ -4,7 +4,7 @@
 void interrupcion(t_process* aProcess, int32_t dir_memoria){
 	t_contenido mensaje;
 	t_header header = recibirMensaje(socketCPU, mensaje, logKernel);
-	if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KRN_INTERRUPCION
+	if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KERNEL_INTERRUPCION
 			log_info(logKernel, "La CPU Me pide hacer una interrupcion :/");
 			agregarProcesoColaBlock(aProcess ->tcb ->pid ,NO_SEMAPHORE);
 
@@ -18,14 +18,11 @@ void bloquear(t_process* aProcess, int32_t id){
 
 		t_contenido mensaje;
 		t_header header = recibirMensaje(socketCPU, mensaje, logKernel);
-		if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KRN_BLOQUEAR
+		if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KERNEL_BLOQUEAR
 				log_info(logKernel, "La CPU Me pide hacer un bloqueo :/");
 				agregarProcesoColaBlock(aProcess ->tcb ->pid ,NO_SEMAPHORE);
 
 			}
-
-
-
 
 }
 
@@ -33,7 +30,7 @@ void crear_hilo(t_process* aProcess){
 
 	t_contenido mensaje;
 			t_header header = recibirMensaje(socketCPU, mensaje, logKernel);
-			if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KRN_CREAR_HILO
+			if(header == CPU_TO_KERNEL_IMPRIMIR_TEXTO){ // CAMBIAR POR CPU_TO_KERNEL_CREAR_HILO
 
 
 				}
