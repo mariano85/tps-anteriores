4fb9aec692776e7d27f40a48ff05d9d8f7ad4507
utnso <utnso@utnso.(none)>
2014-12-19 11:04:45 -0300

    proceso-consola

---
+++ proceso-consola/Debug/consola.d
@@ -0,0 +1,27 @@
+consola.d: ../consola.c ../consola.h \
+ /home/utnso/workspace/commons/commons/log.h \
+ /home/utnso/workspace/commons/commons/config.h \
+ /home/utnso/workspace/commons/commons/collections/dictionary.h \
+ /home/utnso/workspace/commons/commons/collections/node.h \
+ /home/utnso/workspace/commons/commons/sockets.h \
+ /home/utnso/workspace/commons/commons/log.h \
+ /home/utnso/workspace/commons/commons/string.h \
+ /home/utnso/workspace/commons/commons/process.h
+
+../consola.h:
+
+/home/utnso/workspace/commons/commons/log.h:
+
+/home/utnso/workspace/commons/commons/config.h:
+
+/home/utnso/workspace/commons/commons/collections/dictionary.h:
+
+/home/utnso/workspace/commons/commons/collections/node.h:
+
+/home/utnso/workspace/commons/commons/sockets.h:
+
+/home/utnso/workspace/commons/commons/log.h:
+
+/home/utnso/workspace/commons/commons/string.h:
+
+/home/utnso/workspace/commons/commons/process.h:
